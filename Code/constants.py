7f67e2da9e35324fd1d66d55ec18e5410c3ba791
Rohan Tondulkar <rohantondulkar@Rohans-MacBook-Pro.local>
2017-09-01 21:07:23 +0530

    Initial commit containing Text files corpus and stop words.

---
+++ Code/constants.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  1 21:01:49 2017
+
+@author
+Rohan Tondulkar (CS17MTECH11028), Uddipta Bhattacharjee (CS17MTECH11026), Manisha Dubey (CS17RESCH11003)"""
---
+++ Code/tokenizationUtils.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  1 14:41:36 2017
+
+@author
+Rohan Tondulkar (CS17MTECH11028), Uddipta Bhattacharjee (CS17MTECH11026), Manisha Dubey (CS17RESCH11003)"""
